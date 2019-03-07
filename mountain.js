ae77f56cfe50ebf829fea159c8c8608ac107f8d2
dannykrug <dannykrug@gmail.com>
2019-03-07 09:23:34 -0500

    mountains algo

---
+++ mountain.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    let h = 0;
+    let count = 0;
+
+    for (let i = 0; i < n; i++) {
+        if (s.charAt(i) === "D" && h == 0) {
+            count++;
+            h--;
+        } else if (s.charAt(i) === "D" && h !== 0) {
+            h--;
+        } else if (s.charAt(i) === "U") {
+            h++;
+        }
+
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
